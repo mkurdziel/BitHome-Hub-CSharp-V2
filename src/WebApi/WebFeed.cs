8fa98399b2a07db1222cc8f0c5a8a633416b9fd4
Mike Kurdziel <michael.kurdziel@gmail.com>
2013-08-11 01:51:43 -0700

    Feed page automatically updates with data stream values

--- src/WebApi/WebFeed.cs
+++ src/WebApi/WebFeed.cs
@@ -1,12 +1,25 @@
 using System;
+using BitHome.Feeds;
 
-namespace BitHome
+namespace BitHome.WebApi
 {
 	public class WebFeed
 	{
-		public WebFeed ()
+		private Feed m_feed;
+
+		public WebFeed (Feed feed)
 		{
+			m_feed = feed;
 		}
+
+		public long Id { get { return m_feed.Id; } }
+		public String Name { get { return m_feed.Name; } }
+		public bool IsPrivate { get { return m_feed.IsPrivate; } }
+		public String Url { get { return m_feed.Url; } }
+		public FeedStatus Status { get { return m_feed.Status; } }
+		public String Created { get { return m_feed.Created.ToString(); } }
+		public String Updated { get { return m_feed.Updated.ToString (); } }
+		public Version Version { get { return m_feed.Version; } }
 	}
 }
 
