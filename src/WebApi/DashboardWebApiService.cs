28ee4f8be6e77f17407066667bd32bda378d1220
Mike Kurdziel <michael.kurdziel@gmail.com>
2013-08-13 18:20:34 -0700

    Trigger create dialog

--- src/WebApi/DashboardWebApiService.cs
+++ src/WebApi/DashboardWebApiService.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using BitHome.Feeds;
+using NUnit.Framework;
 using ServiceStack.ServiceHost;
 using BitHome.Actions;
 using BitHome.Dashboards;
@@ -54,6 +55,12 @@ namespace BitHome.WebApi
 		public string DashboardId { get; set; }
 	}
 
+    [Route("/api/dashboards/{dashboardId}/values", "GET")]
+    public class WebApiDashBoardValues : IReturn<WebDashboardValue[]> 
+    {
+        public string DashboardId { get; set; }
+    }
+
 	[Route("/api/dashboards/{dashboardId}/name", "POST")]
 	public class WebApiDashBoardName : IReturn<bool> { 
 		public string DashboardId { get; set; }
@@ -101,6 +108,37 @@ namespace BitHome.WebApi
 			}
 			return null;
 		}
+
+        public WebDashboardValue[] Get(WebApiDashBoardValues request)
+        {
+            Dashboard dashboard = ServiceManager.DashboardService.GetDashboard(request.DashboardId);
+
+            List<WebDashboardValue> values = new List<WebDashboardValue>();
+
+            if (dashboard != null)
+            {
+                foreach (String dashboardItemid in dashboard.DashboardItemIds)
+                {
+
+                    DashboardItem item = ServiceManager.DashboardService.GetDashboardItem(dashboardItemid);
+
+                    if (item.FeedId != null) {
+                        Feed feed = ServiceManager.FeedService.GetFeed(item.FeedId);
+                        DataStream[] dataStreams = feed.GetDataStreams();
+
+                        foreach(DataStream datastream in dataStreams) {
+                            values.Add( new WebDashboardValue{
+                                FeedId = item.FeedId.ToString(), 
+                                DataStreamId = datastream.Id, 
+                                Value = datastream.Value});
+                        }
+                    }
+                }
+
+                return values.ToArray();
+            }
+            return null;
+        }
 
 		public DashboardItem Post(WebApiDashboardItemPosition request)
 		{
---
+++ src/WebApi/WebDashboardValue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BitHome.WebApi
+{
+    [Serializable]
+    public class WebDashboardValue
+    {
+        public String FeedId { get; set; }
+        public String DataStreamId { get; set; }
+        public String Value { get; set; }
+    }
+}
